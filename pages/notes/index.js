4c70bfb88aafac4a87e9a6960fe70367994e442f
angad737 <angad737@gmail.com>
2022-01-11 14:59:13 +0530

    Created the Dashboard Starter Template

---
+++ pages/notes/index.js
@@ -0,0 +1,12 @@
+import { Typography } from "antd";
+const { Title, Text } = Typography;
+
+const Notes = (props) => {
+  return (
+    <>
+      <Title level={3}>Notes</Title>
+      <Text></Text>
+    </>
+  );
+};
+export default Notes;
